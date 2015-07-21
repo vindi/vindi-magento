f29078cf81f6e55e69ccd3f47471029af8598658
Ravan Scafi <rscafi@gmail.com>
2015-07-21 11:33:10 -0300

    Updated setup scripts

--- src/app/code/local/Vindi/Subscription/sql/vindi_subscription_setup/install-0.0.1.php
+++ src/app/code/local/Vindi/Subscription/sql/vindi_subscription_setup/install-0.0.1.php
@@ -4,111 +4,115 @@
 $installer = $this;
 $installer->startSetup();
 
-$installer->addAttribute(
-    Mage_Catalog_Model_Product::ENTITY,
-    'vindi_subscription_plan',
-    [
-        'type'                    => 'int',
-        'input'                   => 'select',
-        'backend'                 => '',
-        'frontend'                => '',
-        'label'                   => 'Plano da Vindi',
-        'class'                   => '',
-        'source'                  => 'vindi_subscription/product_attribute_plan',
-        'global'                  => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_STORE,
-        'visible'                 => true,
-        'required'                => true,
-        'user_defined'            => false,
-        'default'                 => '',
-        'searchable'              => false,
-        'filterable'              => false,
-        'comparable'              => false,
-        'visible_on_front'        => false,
-        'unique'                  => false,
-        'apply_to'                => 'subscription',
-        'is_configurable'         => false,
-        'used_in_product_listing' => false,
-        'option'                  => [
-            'values' => [],
-        ],
-    ]
-);
+if (! $this->getAttribute(Mage_Catalog_Model_Product::ENTITY, 'vindi_subscription_plan', 'attribute_id')) {
+    $installer->addAttribute(
+        Mage_Catalog_Model_Product::ENTITY,
+        'vindi_subscription_plan',
+        [
+            'type'                    => 'int',
+            'input'                   => 'select',
+            'backend'                 => '',
+            'frontend'                => '',
+            'label'                   => 'Plano da Vindi',
+            'class'                   => '',
+            'source'                  => 'vindi_subscription/product_attribute_plan',
+            'global'                  => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_STORE,
+            'visible'                 => true,
+            'required'                => true,
+            'user_defined'            => false,
+            'default'                 => '',
+            'searchable'              => false,
+            'filterable'              => false,
+            'comparable'              => false,
+            'visible_on_front'        => false,
+            'unique'                  => false,
+            'apply_to'                => 'subscription',
+            'is_configurable'         => false,
+            'used_in_product_listing' => false,
+            'option'                  => [
+                'values' => [],
+            ],
+        ]
+    );
 
-$attributeId = $installer->getAttributeId(
-    Mage_Catalog_Model_Product::ENTITY,
-    'vindi_subscription_plan'
-);
+    $attributeId = $installer->getAttributeId(
+        Mage_Catalog_Model_Product::ENTITY,
+        'vindi_subscription_plan'
+    );
 
-$defaultSetId = $installer->getAttributeSetId(Mage_Catalog_Model_Product::ENTITY, 'default');
+    $defaultSetId = $installer->getAttributeSetId(Mage_Catalog_Model_Product::ENTITY, 'default');
 
-$installer->addAttributeGroup(
-    Mage_Catalog_Model_Product::ENTITY,
-    $defaultSetId,
-    'Vindi'
-);
-
-//find out the id of the new group
-$groupId = $installer->getAttributeGroup(
-    Mage_Catalog_Model_Product::ENTITY,
-    $defaultSetId,
-    'Vindi',
-    'attribute_group_id'
-);
-
-//assign the attribute to the group and set
-if ($attributeId > 0) {
-    $installer->addAttributeToSet(
+    $installer->addAttributeGroup(
         Mage_Catalog_Model_Product::ENTITY,
         $defaultSetId,
-        $groupId,
-        $attributeId
-    );
-}
-
-$attributes = [
-    'price',
-    'special_price',
-    'special_from_date',
-    'special_to_date',
-    'minimal_price',
-    'cost',
-    'tier_price',
-    'weight',
-    'tax_class_id',
-];
-
-foreach ($attributes as $attributeCode) {
-    $applyTo = explode(
-        ',',
-        $installer->getAttribute(
-            Mage_Catalog_Model_Product::ENTITY,
-            $attributeCode,
-            'apply_to'
-        )
+        'Vindi'
     );
 
-    if (! in_array('subscription', $applyTo)) {
-        $applyTo[] = 'subscription';
-        $installer->updateAttribute(
+    //find out the id of the new group
+    $groupId = $installer->getAttributeGroup(
+        Mage_Catalog_Model_Product::ENTITY,
+        $defaultSetId,
+        'Vindi',
+        'attribute_group_id'
+    );
+
+    //assign the attribute to the group and set
+    if ($attributeId > 0) {
+        $installer->addAttributeToSet(
             Mage_Catalog_Model_Product::ENTITY,
-            $attributeCode,
-            'apply_to',
-            join(',', $applyTo)
+            $defaultSetId,
+            $groupId,
+            $attributeId
         );
+    }
+
+    $attributes = [
+        'price',
+        'special_price',
+        'special_from_date',
+        'special_to_date',
+        'minimal_price',
+        'cost',
+        'tier_price',
+        'weight',
+        'tax_class_id',
+    ];
+
+    foreach ($attributes as $attributeCode) {
+        $applyTo = explode(
+            ',',
+            $installer->getAttribute(
+                Mage_Catalog_Model_Product::ENTITY,
+                $attributeCode,
+                'apply_to'
+            )
+        );
+
+        if (! in_array('subscription', $applyTo)) {
+            $applyTo[] = 'subscription';
+            $installer->updateAttribute(
+                Mage_Catalog_Model_Product::ENTITY,
+                $attributeCode,
+                'apply_to',
+                join(',', $applyTo)
+            );
+        }
     }
 }
 
-$installer->addAttribute('customer', 'vindi_user_code', [
-    'input'          => 'text',
-    'type'           => 'varchar',
-    'is_visible'     => false,
-    'is_unique'      => true,
-    'is_required'    => false,
-    'sort_order'     => 500,
-    'label'          => 'Código do Cliente na Vindi',
-    'used_in_forms'  => ['adminhtml_customer'],
-    'backend_type'   => 'varchar',
-    'default_value'  => '',
-]);
+if (! $this->getAttribute('customer', 'vindi_user_code', 'attribute_id')) {
+    $installer->addAttribute('customer', 'vindi_user_code', [
+        'input'         => 'text',
+        'type'          => 'varchar',
+        'is_visible'    => false,
+        'is_unique'     => true,
+        'is_required'   => false,
+        'sort_order'    => 500,
+        'label'         => 'Código do Cliente na Vindi',
+        'used_in_forms' => ['adminhtml_customer'],
+        'backend_type'  => 'varchar',
+        'default_value' => '',
+    ]);
+}
 
 $installer->endSetup();
--- src/app/code/local/Vindi/Subscription/sql/vindi_subscription_setup/upgrade-0.0.1-0.0.2.php
+++ src/app/code/local/Vindi/Subscription/sql/vindi_subscription_setup/upgrade-0.0.1-0.0.2.php
@@ -2,50 +2,52 @@
 
 $setup = new Mage_Sales_Model_Resource_Setup('core_setup');
 
-$setup->addAttribute(
-    Mage_Sales_Model_Order::ENTITY,
-    'vindi_subscription_id',
-    [
-        'type'             => 'varchar',
-        'input'            => 'text',
-        'backend'          => '',
-        'frontend'         => '',
-        'label'            => 'Id da Assinatura Vindi',
-        'class'            => '',
-        'global'           => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_STORE,
-        'visible'          => false,
-        'required'         => false,
-        'user_defined'     => false,
-        'default'          => '',
-        'searchable'       => false,
-        'filterable'       => false,
-        'comparable'       => false,
-        'visible_on_front' => false,
-        'unique'           => false,
-    ]
-);
-
-$setup->addAttribute(
-    Mage_Sales_Model_Order::ENTITY,
-    'vindi_subscription_period',
-    [
-        'type'             => 'varchar',
-        'input'            => 'text',
-        'backend'          => '',
-        'frontend'         => '',
-        'label'            => 'Período da Assinatura Vindi',
-        'class'            => '',
-        'global'           => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_STORE,
-        'visible'          => false,
-        'required'         => false,
-        'user_defined'     => false,
-        'default'          => '',
-        'searchable'       => false,
-        'filterable'       => false,
-        'comparable'       => false,
-        'visible_on_front' => false,
-        'unique'           => false,
-    ]
-);
-
+if (! $this->getAttribute(Mage_Sales_Model_Order::ENTITY, 'vindi_subscription_id', 'attribute_id')) {
+    $setup->addAttribute(
+        Mage_Sales_Model_Order::ENTITY,
+        'vindi_subscription_id',
+        [
+            'type'             => 'varchar',
+            'input'            => 'text',
+            'backend'          => '',
+            'frontend'         => '',
+            'label'            => 'Id da Assinatura Vindi',
+            'class'            => '',
+            'global'           => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_STORE,
+            'visible'          => false,
+            'required'         => false,
+            'user_defined'     => false,
+            'default'          => '',
+            'searchable'       => false,
+            'filterable'       => false,
+            'comparable'       => false,
+            'visible_on_front' => false,
+            'unique'           => false,
+        ]
+    );
+}
+if (! $this->getAttribute(Mage_Sales_Model_Order::ENTITY, 'vindi_subscription_period', 'attribute_id')) {
+    $setup->addAttribute(
+        Mage_Sales_Model_Order::ENTITY,
+        'vindi_subscription_period',
+        [
+            'type'             => 'varchar',
+            'input'            => 'text',
+            'backend'          => '',
+            'frontend'         => '',
+            'label'            => 'Período da Assinatura Vindi',
+            'class'            => '',
+            'global'           => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_STORE,
+            'visible'          => false,
+            'required'         => false,
+            'user_defined'     => false,
+            'default'          => '',
+            'searchable'       => false,
+            'filterable'       => false,
+            'comparable'       => false,
+            'visible_on_front' => false,
+            'unique'           => false,
+        ]
+    );
+}
 $setup->endSetup();
