5f15eef96bb411e7288dc7ae0c8d1c3be3c50cdd
Tales Galvão <tales.galvao@gmail.com>
2015-10-02 17:20:44 -0300

    Add Default shipping method in configs

---
+++ src/app/code/community/Vindi/Subscription/Model/Config/ShippingMethod.php
@@ -0,0 +1,39 @@
+<?php
+
+class Vindi_Subscription_Model_Config_ShippingMethod
+{
+    public function toOptionArray()
+    {
+        return $this->getActivedShippingMethods();
+    }
+
+
+    public function getActivedShippingMethods()
+    {
+        $methods = Mage::getSingleton('shipping/config')->getActiveCarriers();
+
+        $options = array();
+        $options[] = array('value' => '', 'label' => 'Nenhum');
+
+        foreach($methods as $_ccode => $_carrier)
+        {
+            $_methodOptions = array();
+            if($_methods = $_carrier->getAllowedMethods())
+            {
+                foreach($_methods as $_mcode => $_method)
+                {
+                    $_code = $_ccode . '_' . $_mcode;
+                    $_methodOptions[] = array('value' => $_code, 'label' => $_method);
+                }
+
+                if(!$_title = Mage::getStoreConfig("carriers/$_ccode/title"))
+                    $_title = $_ccode;
+
+                $options[] = array('value' => $_methodOptions[0]['value'], 'label' => $_title);
+            }
+        }
+
+        return $options;
+    }
+
+}
