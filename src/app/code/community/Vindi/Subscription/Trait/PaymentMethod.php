39fd299ae583adccebcc15f9c2188082bf163655
laerte-guimaraes <31661772+laerte-guimaraes@users.noreply.github.com>
2018-08-08 18:51:21 -0300

    Update PaymentMethod.php

--- src/app/code/community/Vindi/Subscription/Trait/PaymentMethod.php
+++ src/app/code/community/Vindi/Subscription/Trait/PaymentMethod.php
@@ -350,14 +350,4 @@ trait Vindi_Subscription_Trait_PaymentMethod
 
         return true;
     }
-
-    protected function validateExpDate($expYear, $expMonth)
-    {
-        $date = Mage::app()->getLocale()->date();
-        return !(!$expYear
-            || !$expMonth
-            || ($date->compareYear($expYear) == 1)
-            || ($date->compareYear($expYear) == 0
-            && ($date->compareMonth($expMonth) == 1)));
-    }
 }
