5e90c1961826654397533de74e784de9d6f4c422
Rubens Takauti <rtakauti@hotmail.com>
2018-09-12 12:59:03 -0300

    Update TestCaseConfiguration.php

--- src/app/code/community/Vindi/Subscription/tests/configuration/Magium/TestCaseConfiguration.php
+++ src/app/code/community/Vindi/Subscription/tests/configuration/Magium/TestCaseConfiguration.php
@@ -5,4 +5,4 @@ $this->capabilities = \Magium\TestCaseConfiguration::CAPABILITIES_CHROME;
 //$this->capabilities = \Magium\TestCaseConfiguration::CAPABILITIES_FIREFOX;
 //$this->capabilities = \Magium\TestCaseConfiguration::CAPABILITIES_PHANTOMJS;
 
-$this->webDriverRemote = 'http://example:4444/wd/hub';
+$this->webDriverRemote = 'http://selenium:4444/wd/hub';
