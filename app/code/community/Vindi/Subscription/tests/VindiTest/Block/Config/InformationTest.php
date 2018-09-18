e829e267ea1a2c4afd9c07df2ad18aa0e0a837b9
Rubens S. Takauti <rtakauti@hotmail.com>
2018-09-18 15:18:58 -0300

    Remove testes

--- app/code/community/Vindi/Subscription/tests/VindiTest/Block/Config/InformationTest.php
+++ app/code/community/Vindi/Subscription/tests/VindiTest/Block/Config/InformationTest.php
@@ -16,32 +16,6 @@ use Magium\Magento\Navigators\Admin\SystemConfiguration;
 class InformationTest extends AbstractMagentoTestCase
 {
     /**
-     * Teste da ativação do módulo da Vindi
-     */
-//    public function testEnablePaymentMethod()
-//    {
-//        $this->getLogger()->notice('Testando a ativação do módulo');
-//        $this->commandOpen($this->getTheme('Admin\ThemeConfiguration')->getBaseUrl());
-//        $this->getAction(Login::ACTION)->login();
-//        $this->getNavigator(AdminMenu::NAVIGATOR)->navigateTo('System/Configuration');
-//        $this->getNavigator(SystemConfiguration::NAVIGATOR)->navigateTo('Payment Methods/Vindi - Cartão de Crédito');
-//        self::assertEquals(1, $this->byId('payment_vindi_creditcard_active')->getAttribute('value'));
-//    }
-
-
-    /**
-     * Teste do registro da API Key da Vindi
-     */
-//    public function testAPiKeyRegistered()
-//    {
-//        $this->getLogger()->notice('Testando o registro da API Key no módulo');
-//        $this->getAction(Login::ACTION)->login();
-//        $this->getNavigator(AdminMenu::NAVIGATOR)->navigateTo('System/Configuration');
-//        $this->getNavigator(SystemConfiguration::NAVIGATOR)->navigateTo('Vindi Assinaturas/Configuração');
-//        $this->assertPageHasText('Conectado com Sucesso!');
-//    }
-
-    /**
      * Teste de SSL do front da loja do magento
      */
     public function testHttpsAssertionFront()
