4793c00fa78418bf3f78fb5b47406764ca555158
Unknown <laerte.guimaraes@vindi.com.br>
2019-02-25 13:11:29 -0300

    Aplica CS

--- app/code/community/Vindi/Subscription/Trait/LogMessenger.php
+++ app/code/community/Vindi/Subscription/Trait/LogMessenger.php
@@ -2,40 +2,40 @@
 
 trait Vindi_Subscription_Trait_LogMessenger
 {
-    /**
-     * Grava os logs no arquivo definido $local
-     *
-     * @param string   $message, $local , int|null $level
-     */
-    public function log($message, $local = 'vindi_exception.log', $level = null)
-    {
-        Mage::log($message, $level, $local);
+	/**
+	 * Grava os logs no arquivo definido $local
+	 *
+	 * @param string   $message, $local , int|null $level
+	 */
+	public function log($message, $local = 'vindi_exception.log', $level = null)
+	{
+		Mage::log($message, $level, $local);
 
-        $this->manageWebhook($level);
-    }
+		$this->manageWebhook($level);
+	}
 
-    /**
-     * Grava o histórico de Webhooks recebidos retornando um Status Code HTTP
-     *
-     * @param string   $message, int|null $level
-     *
-     * @return  bool
-     */
-    public function logWebhook($message, $level)
-    {
+	/**
+	 * Grava o histórico de Webhooks recebidos retornando um Status Code HTTP
+	 *
+	 * @param string   $message, int|null $level
+	 *
+	 * @return  bool
+	 */
+	public function logWebhook($message, $level)
+	{
 	$this->log($message, 'vindi_webhooks.log', $level);
 
-        switch ($level) {
-        case 4:
-            http_response_code(422);
-            return false;
-            break;
-        case 5:
-            return false;
-            break;
-        default:
-            return true;
-            break;
-        }
-    }
+		switch ($level) {
+		case 4:
+			http_response_code(422);
+			return false;
+			break;
+		case 5:
+			return false;
+			break;
+		default:
+			return true;
+			break;
+		}
+	}
 }
