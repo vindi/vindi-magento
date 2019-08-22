434a9b3ea5ed34d6985866cc492e0473e8d55eb2
Laerte Guimarães <laerte.guimaraes@live.com>
2019-08-22 16:47:43 -0300

    Insere testes para compras com assinatura

--- tests/acceptance/vindiCheckoutWithBankSlipCest.php
+++ tests/acceptance/vindiCheckoutWithBankSlipCest.php
@@ -46,4 +46,21 @@ class VindiCheckoutWithBankSlipCest
         if ($bill['amount'] != '14.8')
             throw new \RuntimeException;
     }
+
+    public function buyAnSubscription(AcceptanceTester $I)
+    {
+        $I->setDefaultBankSlip($I);
+        $I->loginAsUser($I);
+        $I->addSubscriptionToCart($I);
+        $I->click('Proceed to Checkout');
+        $I->skipCheckoutForm($I);
+        $I->waitForElement('#dt_method_vindi_bankslip', 30);
+        $I->selectOption('dl#checkout-payment-method-load', 'Boleto Bancário');
+        $I->click('Continue', '#payment-buttons-container');
+        $I->waitForElement('#review-buttons-container', 30);
+        $I->click('Place Order');
+        $I->waitForElement('.main-container.col1-layout', 30);
+        $I->seeInCurrentUrl('/checkout/onepage/success');
+        $I->see('Your order has been received.');
+    }
 }
--- tests/acceptance/vindiCheckoutWithCreditCardCest.php
+++ tests/acceptance/vindiCheckoutWithCreditCardCest.php
@@ -83,4 +83,28 @@ class VindiCheckoutWithCreditCardCest
         if ($bill['amount'] != '14.8')
             throw new \RuntimeException;
     }
+
+    public function buyAnSubscriptionInInstallment(AcceptanceTester $I)
+    {
+        $I->setDefaultCreditCard($I, true);
+        $I->loginAsUser($I);
+        $I->addSubscriptionToCart($I);
+        $I->click('Proceed to Checkout');
+        $I->skipCheckoutForm($I);
+        $I->waitForElement('#dt_method_vindi_creditcard', 30);
+        $I->selectOption('dl#checkout-payment-method-load', 'Cartão de Crédito');
+        $I->waitForElement('#vindi_cc_installments', 30);
+
+        try
+        {
+            $I->fillCreditCardInfo($I, 2);
+        } catch(Exception $e) { }
+
+        $I->click('Continue', '#payment-buttons-container');
+        $I->waitForElement('#review-buttons-container', 30);
+        $I->click('Place Order');
+        $I->waitForElement('.main-container.col1-layout', 30);
+        $I->seeInCurrentUrl('/checkout/onepage/success');
+        $I->see('Your order has been received.');
+    }
 }
