e9b35071949a0c24b69b3ec66204f796f5c96c8f
Laerte Guimarães <laerte.guimaraes@live.com>
2019-08-22 17:57:08 -0300

    Ajuste teste de parcelamento em assinaturas

--- tests/acceptance/vindiCheckoutWithCreditCardCest.php
+++ tests/acceptance/vindiCheckoutWithCreditCardCest.php
@@ -84,7 +84,7 @@ class VindiCheckoutWithCreditCardCest
             throw new \RuntimeException;
     }
 
-    public function buyAnSubscriptionInInstallment(AcceptanceTester $I)
+    public function buyAnMonthlySubscriptionWithInstallments(AcceptanceTester $I)
     {
         $I->setDefaultCreditCard($I, true);
         $I->loginAsUser($I);
@@ -93,13 +93,23 @@ class VindiCheckoutWithCreditCardCest
         $I->skipCheckoutForm($I);
         $I->waitForElement('#dt_method_vindi_creditcard', 30);
         $I->selectOption('dl#checkout-payment-method-load', 'Cartão de Crédito');
-        $I->waitForElement('#vindi_cc_installments', 30);
+        $I->click('Continue', '#payment-buttons-container');
+        $I->waitForElement('#review-buttons-container', 30);
+        $I->click('Place Order');
+        $I->waitForElement('.main-container.col1-layout', 30);
+        $I->seeInCurrentUrl('/checkout/onepage/success');
+        $I->see('Your order has been received.');
+    }
 
-        try
-        {
-            $I->fillCreditCardInfo($I, 2);
-        } catch(Exception $e) { }
-
+    public function buyAnSubscriptionWithoutInstallment(AcceptanceTester $I)
+    {
+        $I->setDefaultCreditCard($I, false);
+        $I->loginAsUser($I);
+        $I->addSubscriptionToCart($I);
+        $I->click('Proceed to Checkout');
+        $I->skipCheckoutForm($I);
+        $I->waitForElement('#dt_method_vindi_creditcard', 30);
+        $I->selectOption('dl#checkout-payment-method-load', 'Cartão de Crédito');
         $I->click('Continue', '#payment-buttons-container');
         $I->waitForElement('#review-buttons-container', 30);
         $I->click('Place Order');
